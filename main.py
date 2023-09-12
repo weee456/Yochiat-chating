9a64e6c92b0632056ac1f2f9e75feba6a6345df5
weee456 <jhweee456@outlook.com>
2023-09-12 09:07:08 +0900

    first commit

--- main.py
+++ main.py
@@ -1,26 +1,65 @@
-from tkinter import *
-from tk_html_widgets import HTMLLabel
+from socket import socket, gethostbyname, gethostname, AF_INET, SOCK_STREAM
+from tkinter import Tk, Frame, Entry, Label, Scrollbar, Button
+from threading import Thread
+import logging
 
-class Main:
+from abc import ABC, abstractmethod
+class AbstractServerClass(ABC):
+    ''' 추상클래스 '''
     def __init__(self):
-        self.items = ['body', 'html', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
-                 'li', 'br', 'ol', 'ul', 'img', 'em', 'a', 'b', 'p',
-                 'div', 'span', 'i', 'u', 'em', 'pre', 'code', 'style', 'href', 'width', 'height', 'type'
-        ]
+        ...
 
-    def show(self, master):
-        master.geometry("640x480-100-100")
-        master.title("Html Editor")
+    @abstractmethod
+    def send_data(self, data):
+        ''' send data method '''
+        ...
+
+    @abstractmethod
+    def recv_data(self):
+        ''' recive data method '''
+        ...
+
+class Server(AbstractServerClass):
+    def start(self, host: str, port: int, name: str):
+        self.sock = socket(AF_INET, SOCK_STREAM)
+        self.sock.bind((host, port))
+        self.sock.listen()
+        self.client, self.addr = self.sock.accept()
+        self.client.sendfile(name.encode)
+        name = self.client.recv(1024).decode()
+        return name
         
-        menubar = Menu(master)
-        filemenu = Menu(master, tearoff=0)
-        filemenu.add_command(label="exit Alt+F4", command=quit)
-        menubar.add_cascade(label="File", menu=filemenu)
-        master.config(menu=menubar)
+    def send_data(self, data):
+        if data:
+            self.client.send(data.encode())
+            return data
 
-        body = Button(master, text="body")
-        body.pack()        
+    def recv_data(self):
+        while 1:
+            data = self.client.recv(1024).decode()
+            if not data:
+                break
+            return data
 
-root = Tk()
-Main().show(root)
-root.mainloop()
+class Client(AbstractServerClass):
+    def start(self, host: str, port: int, name: str):
+        self.client_sock = socket(AF_INET, SOCK_STREAM)
+        self.client_sock.connect(host, port)
+        self.client_sock.sendfile(name.encode())
+        name = self.client_sock.recv(1024).decode()
+        return name
+    
+if __name__ == "__main__":
+    host = gethostbyname(gethostname())
+    port = 9063
+
+    server = Server()
+    server.start(host=host, port=port, name='hh')
+    server.send_data()
+    server.recv_data()
+
+    
+    client = Client()
+    client.connect(host=host, port=port, name='tt')
+    client.send_data()
+    client.recv_data()
